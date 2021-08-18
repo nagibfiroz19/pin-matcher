873d70e9dac6ad926c07c292621a82b336dca078
nagibfiroz19 <nagibfiroz.seu19@gmail.com>
2021-08-19 02:21:54 +0600

    added

--- pin-matcher/js/index.js
+++ pin-matcher/js/index.js
@@ -1,15 +1,32 @@
-function getPin(){
-    const pin= Math.round( Math.random ()*10000); 
-    const pinString=pin+'';
-    if(pinString.length==4){
-        return pin;
-    }
-    else{
-        return getPin();
-    }
-}
+    // for generate pin part
 
-function generatePin(){
-       document.getElementById('display-pin').value=getPin();
-  
-}
+    function getPin() {
+        const pin = Math.round(Math.random() * 10000);
+        const pinString = pin + '';
+        if (pinString.length == 4) {
+            return pin;
+        } else {
+            return getPin();
+        }
+    }
+
+    function generatePin() {
+        document.getElementById('display-pin').value = getPin();
+
+    }
+
+    //for type the correct pin number in display
+    document.getElementById('key-pad').addEventListener('click', function (event) {
+        const number = event.target.innerText;
+        const calcInput = document.getElementById('disply-input');
+        if (isNaN(number)) {
+            if (number == 'C') {
+                calcInput.value = '';
+            }
+        } else {
+            const previousNumber = calcInput.value;
+            const newNumber = previousNumber + number;
+            calcInput.value = newNumber;
+
+        }
+    })
